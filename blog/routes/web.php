98b4c95cb450b37d5f180fee6b623941c834fbc0
Paul Ferreira <paul.ferreira@epita.fr>
2023-06-12 18:49:26 +0700

    debug

--- blog/routes/web.php
+++ blog/routes/web.php
@@ -32,7 +32,7 @@ Auth::routes();
 Route::get('/home', [HomeController::class, 'index'])->name('home');
 Route::get('/member_process',[MemberProcessController::class, 'init'])->name('member.init');
 Route::get('/list_class_process',[ListClassController::class, 'init'])->name('list_class.init');
-Route::get('/list_class_process',[ListClassTrainerController::class, 'init'])->name('list_class_trainer.init');
+Route::get('/list_class_trainer_process',[ListClassTrainerController::class, 'init'])->name('list_class_trainer.init');
 Route::get('/list_machine_process',[ListMachineController::class, 'init'])->name('list_machine.init');
 Route::get('/check_in_process',[CheckInController::class, 'init'])->name('check_in.init');
 
@@ -48,24 +48,24 @@ Route::group(['middleware' => ['auth']], function() {
 
 Route::get('/trainer', function () {
     return view('trainer');
-});
+})->name('trainer');
 
 Route::get('/member', function () {
-    return view('Member.member');
+    return view('member');
 })->name('member');
 
 Route::get('/class_list', function () {
-    return view('Member.classlist');
+    return view('classlist');
 })->name('classlist');
 
 Route::get('/class_list_trainer', function () {
-    return view('Trainer.classlisttrainer');
+    return view('classlisttrainer');
 })->name('classlisttrainer');
 
 Route::get('/machine_list', function () {
-    return view('Trainer.machinelist');
+    return view('machinelist');
 })->name('machinelist');
 
 Route::get('/check_in_history', function () {
-    return view('Member.checkinhistory');
+    return view('checkinhistory');
 })->name('checkin');
