b0efe7aafd915bfbd093381b123762379e98f6e1
theo <theo.oliver@epita.fr>
2023-06-14 16:58:07 +0700

    uifix

--- blog/resources/views/layouts/app.blade.php
+++ blog/resources/views/layouts/app.blade.php
@@ -13,10 +13,58 @@
     <link href="https://fonts.googleapis.com/css?family=Raleway:300,400,600" rel="stylesheet" type="text/css">
     <!-- Styles -->
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    <style>
+        .header {
+	 padding: 10px 16px;
+	 position: sticky;
+	 top: 0;
+	 background: #000;
+	 color: #ffffff;
+	}
+		
+	.title_header
+	{
+	 color: #ffffff;
+	 -webkit-animation: glow 1s ease-in-out infinite alternate;
+  	 -moz-animation: glow 1s ease-in-out infinite alternate;
+ 	 animation: glow 1s ease-in-out infinite alternate;
+	}
+	
+	@-webkit-keyframes glow {
+  	from {
+   	  text-shadow: 0 0 10px #fff, 0 0 20px #fff, 0 0 30px #e60073, 0 0 40px #e60073, 0 0 50px #e60073, 0 0 60px #e60073, 0 0 70px #e60073;
+ 	 }
+ 	 to {
+   	  text-shadow: 0 0 20px #fff, 0 0 30px #ff4da6, 0 0 40px #ff4da6, 0 0 50px #ff4da6, 0 0 60px #ff4da6, 0 0 70px #ff4da6, 0 0 80px #ff4da6;
+	 }
+	}
+    </style>
 </head>
 <body>
     <div id="app">
         <main class="py-4">
+		<div class="header" id="myHeader">
+	  	    	<h1 class="title_header">GYM INSHAPE</h1>
+	  	    	@if (Route::has('login'))
+		        <div class="login_register" style="position: absolute;right:20px;top:30px">
+		            @auth
+								<form action="{{ route('home') }}">
+		 		    	<button type="submit">Home</button>
+		            @else
+		                <form action="{{ route('login') }}">
+		 		    <button type="submit">Log in</button>
+	      			</form>
+
+		                @if (Route::has('register'))
+		                    <form action="{{ route('register') }}">
+		 		    	<button type="submit">Register</button>
+	      			    </form>
+
+		                @endif
+		            @endauth
+		        </div>
+		    @endif
+		</div>
             <div class="container">
             @yield('content')
             </div>
--- blog/resources/views/trainer.blade.php
+++ blog/resources/views/trainer.blade.php
@@ -6,9 +6,38 @@
         <div class="col-md-8">
             <div class="card">
                 <div class="card-body">
+                    <style>
+                        .button {
+	  		 background-color: #FFC0CB; /* Green */
+	  		 border: none;
+			 color: white;
+			 padding: 16px 32px;
+			 text-align: center;
+			 text-decoration: none;
+			 display: inline-block;
+			 font-size: 16px;
+			 margin: 4px 2px;
+			 transition-duration: 0.4s;
+			 cursor: pointer;
+			}
+		 	.button_sub {
+			  background-color: white; 
+			  color: black; 
+			  border: 2px solid #FFC0CB;
+			}
+
+			.button_sub:hover {
+			  background-color: #FFC0CB;
+			  color: white;
+			}
+                    </style>
                     <h1> You're logging as a Trainer</h1>
-                    <a href="{{ route('machinelist') }}">My Machines</a>
-                    <a href="{{ route('classlisttrainer') }}">My Classes</a>
+                    <form action="{{ route('machinelist') }}">
+		 	<button class="button button_sub">My Machines</button>
+	      	    </form>
+	      	    <form action="{{ route('classlisttrainer') }}">
+		 	<button class="button button_sub">My Classes</button>
+	      	    </form>
                 </div>
             </div>
         </div>
